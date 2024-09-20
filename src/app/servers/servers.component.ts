e9d4fd5a2614f40151f3d5ad03898ddc7d9ff1ac
mansi-sahagal <mansisahagal3@gmail.com>
2024-09-20 20:21:33 +0530

    ghj

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,16 +1,30 @@
 import { Component,OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector:'[app-servers]' //by custom attribute
+  // selector:'.app-servers'  // by class
+  selector: 'app-servers',   // by unique selector
   // standalone: true,
   // imports: [],
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent implements OnInit {
-  constructor(){}
-  ngOnInit() {
-    
-  }
+allowNewServer=false;
+serverCreationStatus='No server is created';
+serverName='Testserver';
 
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000);
+  }
+  ngOnInit() {
+  }
+  onCreateServer(){
+    this.serverCreationStatus='server is created';
+  }
+  onUpdatedServerName(event:Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
 }
